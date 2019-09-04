974e0d172e775fcb00779b094f21085f0f9269ec
zeus <oh_zec2@hotmail.com>
2019-09-04 20:09:12 -0300

    Aula 3

---
+++ Aula2/listagem.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from pessoa import Pessoa
+class listagem(object):
+    a = Pessoa("Or")
+
+    print('Pos ', a.)
--- Aula2/pessoa.py
+++ Aula2/pessoa.py
@@ -4,5 +4,22 @@ class Pessoa():
         self.end = end
         self.telefone = tel
 if __name__ == "__main__":
-    joao = Pessoa("Zeus Sc","Bonifácil",99876543)
+    joao = Pessoa("Zeus Sc","Bonifacil",99876543)
     print(joao.nome,joao.end,joao.telefone)
+
+    def posOr(self, varPos):
+        livros = ['Oracle', 'Java', 'SqlServer', 'Delphi', 'Python', 'Android', 'Oracle']
+        print(livros.count('Oracle'))
+        busca = varPos
+        for i, livro in enumerate(livros):
+            if busca in livro:
+                print(i, livro)
+livros = ['Oracle', 'Java', 'SqlServer', 'Delphi', 'Python', 'Android', 'Oracle']
+print(livros.count('Oracle'))
+busca = 'Or'
+for i, livro in enumerate(livros):
+    if busca in livro:
+        print(i, livro)
+
+for i, livro in enumerate(livros):
+    print('O livro ' + livro + ' esta na posicao:', i)        
